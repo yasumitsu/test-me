09aeb1716bfb6ea0e8d7cac09fa5ea88c0b9f513
Kenji <kenjiyasumitsu@gmail.com>
2021-09-21 12:32:58 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const Runner = require('./runner');
+
+const runner = new Runner();
+
+const run = async () => {
+	const results = await runner.collectFiles(process.cwd());
+};
